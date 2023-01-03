4b9c5305e205525a2031ef277278dcb0b8f70f84
Chen Sun <sunchen8635@gmail.com>
2023-01-03 19:44:28 +0200

    edits of the axion (master) branch, ipynb, analysis.py, inputs

--- cosmo_axions_analysis.py
+++ cosmo_axions_analysis.py
@@ -90,7 +90,14 @@ if __name__ == '__main__':
     plt.figure(0)
     # labels = keys
     # labels = [r"$\Omega_\Lambda$", r"$h$", r"$\log\ m_a$", r"$\log\ g_a$"]
-    labels = [r"$\Omega_\Lambda$", r"$h$"]
+    labels = []
+    print("keys:", keys)
+    if 'OmL' in keys:
+        labels.append(r"$\Omega_\Lambda$")
+    if 'h0' in keys:
+        labels.append(r"$h$")
+    if 'w' in keys:
+        labels.append(r"$w$")
     if 'logma' in keys:
         labels.append(r"$\log\ m_a$")
     if 'logga' in keys:
@@ -117,15 +124,64 @@ if __name__ == '__main__':
     plt.savefig(pltpath(directory))
 
     # focusing on ma-ga
-    plt.figure(1)
+
     reduced_labels = [r"$\log\ m_a$", r"$\log\ g_a$"]
+    # reduced_samples = samples[:, 2:4]
     reduced_samples = samples[:, 2:4]
     reduced_dim = len(reduced_labels)
 
+    # focusing on one contour 2sigma
+    plt.figure(1)
     figure = corner.corner(reduced_samples,
                            labels=reduced_labels,
                            quantiles=[0.16, 0.5, 0.84],
-                           color='r', show_titles=True,
+                           color='b', show_titles=True,
+                           plot_datapoints=False,
+                           plot_density=False,
+                           fill_contours=True,
+                           # levels=[1.-np.exp(-(2.)**2 /2.)],
+                           levels=[0.68, 0.95, 0.997],
+                           title_kwargs={"fontsize": 12},
+                           label_kwargs={"fontsize": 7},
+                           hist_kwargs={'color': None},
+                           contour_kwargs={'alpha': 0},
+                           labelpad=-0.1)
+
+    # print("figure.axes type is:", type(figure.axes))
+    # print("figure.axes:", np.shape(figure.axes))
+    # figure.axes = (figure.axes[2],)
+    # print("figure.axes:", np.shape(figure.axes))
+
+    # remove 1D posterior
+    axes = np.array(figure.axes).reshape((reduced_dim, reduced_dim))
+    print("shape of axes", axes.shape)
+    for ax in axes[np.triu_indices(reduced_dim)]:
+        ax.remove()
+
+    for ax in figure.get_axes():
+        # shrink tick size
+        ax.tick_params(labelsize=7)
+        # the following will not have any effects as
+        # it's the same as passing labelpad into label_kwargs.
+        # According to the doc of corner.py,
+        # "Note that passing the labelpad keyword in this
+        # dictionary will not have the desired effect. Use
+        # the labelpad keyword in this function instead.
+
+        # ax.xaxis.labelpad = 0
+        # ax.yaxis.labelpad = 0
+
+    # # saving the points of the 1/2/3 sigma contours
+    # np.savetxt(pltpath(directory, head='corner_pts_2sigma_', ext='.txt'), v)
+    plt.savefig(pltpath(directory, head='custom'),
+                bbox_inches='tight')  # , bbox_inches=0)
+
+    # 95 CL
+    plt.figure(2)
+    figure = corner.corner(reduced_samples,
+                           labels=reduced_labels,
+                           quantiles=[0.16, 0.5, 0.84],
+                           color='b', show_titles=True,
                            plot_datapoints=False,
                            plot_density=False,
                            # levels=[1.-np.exp(-(2.)**2 /2.)],
@@ -138,6 +194,46 @@ if __name__ == '__main__':
     v = p.vertices
 
     # saving the points of the 95% C.R. contour
-    np.savetxt(pltpath(directory, head='corner_pts', ext='.txt'), v)
+    np.savetxt(pltpath(directory, head='corner_pts_2sigma_', ext='.txt'), v)
 
-    plt.savefig(pltpath(directory, head='custom'))
+    # other CL
+    # focusing on one contour 3sigma
+    plt.figure(3)
+    figure = corner.corner(reduced_samples,
+                           labels=reduced_labels,
+                           quantiles=[0.16, 0.5, 0.84],
+                           color='r', show_titles=True,
+                           plot_datapoints=False,
+                           plot_density=False,
+                           # levels=[1.-np.exp(-(2.)**2 /2.)],
+                           levels=[0.997],
+                           title_kwargs={"fontsize": 12},
+                           hist_kwargs={'color': None})
+    axes = np.array(figure.axes).reshape((reduced_dim, reduced_dim))
+
+    p = (figure.axes)[2].collections[0].get_paths()[0]
+    v = p.vertices
+
+    # saving the points of the 99.7% C.R. contour
+    np.savetxt(pltpath(directory, head='corner_pts_3sigma_', ext='.txt'), v)
+
+    # other CL
+    # focusing on one contour 1sigma
+    plt.figure(4)
+    figure = corner.corner(reduced_samples,
+                           labels=reduced_labels,
+                           quantiles=[0.16, 0.5, 0.84],
+                           color='r', show_titles=True,
+                           plot_datapoints=False,
+                           plot_density=False,
+                           # levels=[1.-np.exp(-(2.)**2 /2.)],
+                           levels=[0.68],
+                           title_kwargs={"fontsize": 12},
+                           hist_kwargs={'color': None})
+    axes = np.array(figure.axes).reshape((reduced_dim, reduced_dim))
+
+    p = (figure.axes)[2].collections[0].get_paths()[0]
+    v = p.vertices
+
+    # saving the points of the 68% C.R. contour
+    np.savetxt(pltpath(directory, head='corner_pts_1sigma_', ext='.txt'), v)
